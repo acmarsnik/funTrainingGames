b2dd18344c5a074a937ed648c345361962c6d4fb
Albert Marsnik <albertmarsnik@gmail.com>
2017-11-01 21:54:45 +0100

    Added main window and dialog ui files.

---
+++ Classes/gameType.py
@@ -0,0 +1,8 @@
+try:
+    class GameType:
+        def __init__(self):
+            self.id = None
+            self.description = None
+
+except Exception as e:
+    print(e)
---
+++ Classes/user.py
@@ -0,0 +1,9 @@
+try:
+    class User:
+        def __init__(self):
+            self.id = None
+            self.username = None
+            self.password = None
+
+except Exception as e:
+    print(e)
